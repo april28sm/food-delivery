8c732b6921c8761f597d0d75317f302152216027
april28sm <94352502+april28sm@users.noreply.github.com>
2022-11-22 09:38:45 +0900

    commit

---
+++ frontend/src/components/WrappedCommand.vue
@@ -0,0 +1,60 @@
+<template>
+
+    <v-card outlined>
+        <v-card-title>
+            Wrapped
+        </v-card-title>
+
+        <v-card-text>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="wrapped"
+            >
+                Wrapped
+            </v-btn>
+            
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'WrappedCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+        },
+        watch: {
+        },
+        methods: {
+            wrapped() {
+                this.$emit('wrapped', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
